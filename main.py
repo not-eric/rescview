7ff66eb0704994b87c5aace8462f6f6073931a49
Eric Dale <not.eric.dale@gmail.com>
2021-01-04 23:51:02 -0800

    Updated directory, added license
    Beginning to add a project structure, and chose the GNU GPLv3 license for this project.

--- main.py
+++
@@ -1,6 +0,0 @@
-# rescview resource viewer
-# Eric Dale, 2020
-
-def main():
-    """Main method until graphic interface is added."""
-    print("Initial test.")
---
+++ rescview/main.py
@@ -0,0 +1,9 @@
+# rescview resource viewer
+# Eric Dale, 2020
+
+import psutil
+
+
+def main():
+    """Main method until graphic interface is added."""
+    print("Initial test.")
